f657a8905a9452895654f6d51d1d1d88d3b27c35
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-23 21:19:10 -0300

    Initial Commit

---
+++ LinkedInMobileApp/LinkedInMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInMobileAppApp.swift
+//  LinkedInMobileApp
+//
+//  Created by Raphael Cerqueira on 23/09/20.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
